86f5c749a86999635182eeae60ad12c119cb3147
Georg Federmann <georg.federmann@gmail.com>
2019-12-19 00:23:07 +0100

    fix syntax error

--- src/io/jenkins/pipeline/sample/SourceScript1.groovy
+++ src/io/jenkins/pipeline/sample/SourceScript1.groovy
@@ -2,6 +2,6 @@ import io.jenkins.pipeline.sample.SourceScript2
 
 class SourceScript1 {
     def sourceScript1Method() {
-        "sourceScript1 says hi" + " and " + new SourceScript2.sourceScript2Method()
+        "sourceScript1 says hi" + " and " + new SourceScript2().sourceScript2Method()
     }
 }
